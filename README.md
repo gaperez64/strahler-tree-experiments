1b21ae4f1dc0b038e4785d4235be65e0dfbbf01f
vanessaflugel <vanessa.fluegel@gmx.de>
2025-11-21 15:06:22 +0100

    Added p-level-successor code and some explanations in README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# strahler-tree-experiments
+# Strahler Tree Experiments
 Repository to hold some files to play around with Strahler trees.
+
+## Labels in Code
+Labels refer to conditions in page 19 of [this paper](https://arxiv.org/pdf/2003.08627):  
+* _Cases where the siblings does not exist_
+    * **A**: the number of non-empty strings among bitstrings h-1 and r+1 is k-1
+    * **B**: the number of non-leadings bits in bitstrings h-1 to r+1 is t
+    * **C**: bitstring r is 01^j for some j>=0, the number of non-leading bits used in bitstrings h-1 to r is t and all bitstrings r to 1 are non-empty
+    * **D**: bitstring r is 1^j for some j>=1, and the number of non-leading bits used in bitstrings h-1 to r is t
+
+* _Finding the sibling_
+    * **E**: Less than t non-leading bits are used in bitstrings h-1 to r -> append 10^j with j>=0 to original string r, so that exactly t non-leading bits are used
+    * **F**: Exactly t non-leading bits are used in bitstrings h-1 to r and r is in format b01^j for some j>=0 -> use b
+
+* _Setting the remaining bits_
+    * **G**: Set 00^j for some i>=0 so the total number of bits used is (k-1)+t
+    * **H**: Add strings 0 so the number of non-empty bitstrings is k-1
---
+++ str-tree.cc
@@ -0,0 +1,210 @@
+#include <vector>
+#include <unordered_set>
+#include <iostream>
+
+int k = 3, t = 2, h = 2;
+std::vector<std::vector<int>> leaves_b {
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0, 1},
+    {0, 0, 1, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0, 1},
+    {0, 0},
+    {0, 0, 1, 0},
+    {0, 0, 1},
+    {0, 0, 1, 1},
+    {0, 1, 0, 0},
+    {0, 1, 0, 0},
+    {0, 1, 0},
+    {0, 1, 0, 1},
+    {0, 1, 1, 0}
+};
+std::vector<std::vector<int>> leaves_d {
+    {0, 0, 0, 1},
+    {0, 0, 1, 1},
+    {0, 0, 1},
+    {0, 0, 1, 1},
+    {0, 0, 0, 1},
+    {0, 1, 1, 1},
+    {0, 1, 1},
+    {0, 1, 1, 1},
+    {0, 1},
+    {0, 1, 1, 1},
+    {0, 1, 1},
+    {0, 1, 1, 1},
+    {0, 0, 0, 1},
+    {0, 0, 1, 1},
+    {0, 0, 1},
+    {0, 0, 1, 1},
+    {0, 0, 0, 1}
+};
+
+std::vector<int> tmp_b;
+std::vector<int> tmp_d;
+
+int skipUntilNextLevel (std::vector<int>& curr_d, int i) 
+{
+    while ((i >= 0 && curr_d[i] == curr_d[i+1]) || i == curr_d.size() - 1) 
+    {
+        tmp_b[i] = 0;
+        i --;
+    }
+    return i;
+}
+
+void getLevelPSuccessor (int idx, int p)
+{
+    tmp_b = leaves_b[idx];
+    tmp_d = leaves_d[idx];
+
+    int i = tmp_d.size() - 1;
+    std::cout << "Start i in " << i << std::endl;
+    std::cout << "Skipping bits below p\n";
+    // skip bits "below p"
+    while (i >= 0 && tmp_d[i] < p) 
+    {
+        tmp_b[i] = 0;
+        i --;
+    }
+    std::cout << "After skipping i is " << i << std::endl;
+
+    std::cout << "Calculating NES ";
+    // Calculate number of Non-Empty Strings (NES): count unique values in tmp_d up until there
+    size_t nes = std::unordered_set<int>( tmp_d.begin(), tmp_d.begin() + i + 1 ).size();
+    std::cout << nes << std::endl;
+    if (nes == k - 1) {
+        // A: No next sibling on this layer
+        std::cout << "Skipping a level\n";
+        i = skipUntilNextLevel(tmp_d, i);
+    }
+
+    while (i >= 0) {
+        // check if there was a level change
+        if (tmp_d[i] != tmp_d[i+1]) 
+        {
+            nes --;
+            std::cout << "Handling level change\n";
+            // Calculate the Non-Leading Bits (NLB): take the complete length and subtract the number of NES (every NES has one leading bit)
+            int nlb = (i + 1) - nes;
+            std::cout << "NLB " << nlb << " i " << i << " NES " << nes << std::endl;
+            if (nlb < t) 
+            {
+                std::cout <<  "Smaller than t\n";
+                int new_index = tmp_d[i];
+                i ++;
+                tmp_b[i] = 1;
+                tmp_d[i] = new_index;
+                int j = 1;
+                while (nlb + j < t) 
+                {
+                    tmp_b [i + j] = 0;
+                    tmp_d [i + j] = new_index;
+                    j ++;
+                }
+                // remember the last changed position
+                i += j-1;
+                break;
+            }
+            else
+            {
+                i --;
+            }
+
+        }
+        // For all following cases we know NLB == t
+        else if (i == 0 && tmp_b[i] == 1)
+        {
+            std::cout << "We are at top\n";
+            // A base case, we have reached the root and everything is just 1: we are top
+            // Special case of D
+            tmp_d[0] = -1;
+            return;
+        }
+        else if (tmp_b[i] == 0) 
+        {
+            if (i == 0 || tmp_d[i - 1] != tmp_d[i])
+            {
+                std::cout << "Found a 0 in the beginning\n";
+                // The 0 is either the first bit in total, or it is the first bit of that level
+                int strings_after_current = std::unordered_set<int> (tmp_d.begin() + i, tmp_d.end()).size();
+                if (strings_after_current == h - tmp_d[i])
+                {
+                    // All bitstrings after the current level are non-empty, we simply move on
+                    // C: No sibling on this layer
+                    i --;
+                    continue;
+                }
+                // We can use this level: Break out of the loop and start changing at the current i
+                break;
+            }
+            else {
+                assert (tmp_d[i - 1] == tmp_d[i]);
+                std::cout << "A zero in the middle!\n";
+                break;
+            }
+        }
+        else 
+        {
+            std::cout << "start setting things to 0\n";
+            // We can already start setting everything there to 0 and change where it belongs later
+            tmp_b[i] = 0;
+            i --;
+        }
+    }
+
+    // Change where the bits belong
+    std::cout << "Adjusting bit level\n";
+    std::cout << "starting at " << i << std::endl; 
+    
+    int no_of_needed_nes = k-1 - nes;
+    int set_index = tmp_d[i] + 1;
+    i ++;
+    // Fill up with just the next one as long as we still have bits
+    std::cout << "Needed nes: " << no_of_needed_nes << std::endl;
+    while (tmp_b.size() - i > no_of_needed_nes) 
+    {
+        assert (i >= 0 && i < tmp_d.size());
+        tmp_d[i] = set_index;
+        i ++;
+    }
+    std::cout << "Filling singles\n";
+    // Now assign the rest of the bits one level a piece
+    while (i < tmp_b.size())
+    {
+        tmp_d[i] = set_index;
+        i ++;
+        set_index ++;
+    }
+}
+
+int main()
+{
+    for (size_t idx = 0; idx < leaves_b.size(); idx++)
+    {
+        getLevelPSuccessor(idx, h-1);
+        if (tmp_b == leaves_b[idx+1] && tmp_d == leaves_d[idx+1])
+        {
+            std::cout << idx << ": Correct successor!\n";
+        }
+        else 
+        {
+            std::cout << idx << ": Wrong successor!\n";
+        }
+    }
+   /*getLevelPSuccessor(0, h-1);
+   std::cout << "Result: ";
+   for (auto &&i : tmp_b)
+   {
+        std::cout << i;
+   }
+   std::cout << " ";
+   for (auto &&i : tmp_d)
+   {
+        std::cout << i;
+   }
+   std::cout << std::endl;*/
+   
+}
