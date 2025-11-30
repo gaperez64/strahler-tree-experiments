fa7b3a3bc89e18c35eb2a5a5ac86e9eb68a25184
Guillermo A. Perez <gaperez64@gmail.com>
2025-11-30 20:51:28 +0100

    made the stack a bit more flexible

--- genstree.c
+++ genstree.c
@@ -16,22 +16,22 @@ typedef struct _node {
   short u;
 } Node;
 
-void push(short u, int k, int t, int h, size_t* lenq, size_t* maxq, Node** stack) {
-  if (*lenq >= *maxq) {
-    (*maxq) = 2 * ((*maxq) + 1);
-    (*stack) = realloc(*stack, (*maxq) * sizeof(Node));
-    assert (*lenq < *maxq);
-  }
-  Node* fresh = *stack + *lenq;
-  fresh->u = (u == 0);
-  fresh->k = k;
-  fresh->t = t;
-  fresh->h = h;
-  (*lenq)++;
-  assert (*lenq <= *maxq);
-}
+#define PUSH(UVAL, KVAL, TVAL, HVAL, LENQ, MAXQ, STACK)                        \
+  do {                                                                         \
+    if (LENQ >= MAXQ) {                                                        \
+      MAXQ = 2 * (MAXQ + 1);                                                   \
+      STACK = realloc(STACK, MAXQ * sizeof(*STACK));                           \
+      assert(LENQ < MAXQ);                                                     \
+    }                                                                          \
+    STACK[LENQ].u = ((UVAL) == 0);                                             \
+    STACK[LENQ].k = KVAL;                                                      \
+    STACK[LENQ].t = TVAL;                                                      \
+    STACK[LENQ].h = HVAL;                                                      \
+    LENQ++;                                                                    \
+    assert(LENQ <= MAXQ);                                                      \
+  } while (0)
 
-void strahler_tree(int k, int t, int h) {
+unsigned strahler_tree(int k, int t, int h) {
   assert(h >= k);
   unsigned (*tree)[k + 1][t + 1][h + 1] = calloc(2, sizeof(*tree));
   const short UTREE = 0;
@@ -39,63 +39,63 @@ void strahler_tree(int k, int t, int h) {
 
   size_t maxq = 0;
   size_t lenq = 0;
-  Node* stack = nullptr;
+  Node *stack = nullptr;
 
   // this is the node of interest
-  push(0, k, t, h, &lenq, &maxq, &stack);
+  PUSH(0, k, t, h, lenq, maxq, stack);
 
   while (lenq > 0) {
-    Node* cur = stack + lenq - 1;
-    if (cur->u && cur->h == 1 && cur->k == 1) {
-      tree[UTREE][cur->k][cur->t][cur->h] = 1;
+    Node tos = stack[lenq - 1];
+    if (tos.u && tos.h == 1 && tos.k == 1) {
+      tree[UTREE][tos.k][tos.t][tos.h] = 1;
       lenq--; // pop
-    } else if (cur->u && cur->h > 1 && cur->k == 1) {
-      unsigned son = tree[UTREE][cur->k][cur->t][cur->h - 1];
+    } else if (tos.u && tos.h > 1 && tos.k == 1) {
+      unsigned son = tree[UTREE][tos.k][tos.t][tos.h - 1];
       if (son > 0) {
-        tree[UTREE][cur->k][cur->t][cur->h] = son;
+        tree[UTREE][tos.k][tos.t][tos.h] = son;
         lenq--; // pop
       } else {
-        push(0, cur->k, cur->t, cur->h - 1, &lenq, &maxq, &stack);
+        PUSH(0, tos.k, tos.t, tos.h - 1, lenq, maxq, stack);
       }
-    } else if (cur->h >= cur->k && cur->k >= 2 && cur->t == 0) {
-      unsigned son = tree[UTREE][cur->k - 1][cur->t][cur->h - 1];
+    } else if (tos.h >= tos.k && tos.k >= 2 && tos.t == 0) {
+      unsigned son = tree[UTREE][tos.k - 1][tos.t][tos.h - 1];
       if (son > 0) {
-        tree[cur->u ? 0 : 1][cur->k][cur->t][cur->h] = son;
+        tree[tos.u ? 0 : 1][tos.k][tos.t][tos.h] = son;
         lenq--; // pop
       } else {
-        push(0, cur->k - 1, cur->t, cur->h - 1, &lenq, &maxq, &stack);
+        PUSH(0, tos.k - 1, tos.t, tos.h - 1, lenq, maxq, stack);
       }
-    } else if (!cur->u && cur->h >= cur->k && cur->k >= 2 && cur->t >= 1) {
-      unsigned child1 = tree[VTREE][cur->k][cur->t - 1][cur->h];
-      unsigned child2 = tree[UTREE][cur->k - 1][cur->t][cur->h - 1];
+    } else if (!tos.u && tos.h >= tos.k && tos.k >= 2 && tos.t >= 1) {
+      unsigned child1 = tree[VTREE][tos.k][tos.t - 1][tos.h];
+      unsigned child2 = tree[UTREE][tos.k - 1][tos.t][tos.h - 1];
       if (child1 > 0 && child2 > 0) {
-        tree[VTREE][cur->k][cur->t][cur->h] = child1 * 2 + child2;
+        tree[VTREE][tos.k][tos.t][tos.h] = child1 * 2 + child2;
         lenq--; // pop
       } else {
-       push(1, cur->k, cur->t - 1, cur->h, &lenq, &maxq, &stack);
-       push(0, cur->k - 1, cur->t, cur->h - 1, &lenq, &maxq, &stack);
+        PUSH(1, tos.k, tos.t - 1, tos.h, lenq, maxq, stack);
+        PUSH(0, tos.k - 1, tos.t, tos.h - 1, lenq, maxq, stack);
       }
-    } else if (cur->u && cur->h == cur->k && cur->k >= 2) {
-      unsigned son = tree[VTREE][cur->k][cur->t][cur->h];
+    } else if (tos.u && tos.h == tos.k && tos.k >= 2) {
+      unsigned son = tree[VTREE][tos.k][tos.t][tos.h];
       if (son > 0) {
-        tree[UTREE][cur->k][cur->t][cur->h] = son;
+        tree[UTREE][tos.k][tos.t][tos.h] = son;
         lenq--; // pop
       } else {
-        push(1, cur->k, cur->t, cur->h, &lenq, &maxq, &stack);
+        PUSH(1, tos.k, tos.t, tos.h, lenq, maxq, stack);
       }
-    } else if (cur->u && cur->h > cur->k && cur->k >= 2) {
-      unsigned child1 = tree[VTREE][cur->k][cur->t][cur->h];
-      unsigned child2 = tree[UTREE][cur->k][cur->t][cur->h - 1];
+    } else if (tos.u && tos.h > tos.k && tos.k >= 2) {
+      unsigned child1 = tree[VTREE][tos.k][tos.t][tos.h];
+      unsigned child2 = tree[UTREE][tos.k][tos.t][tos.h - 1];
       if (child1 > 0 && child2 > 0) {
-        tree[UTREE][cur->k][cur->t][cur->h] = child1 * 2 + child2;
+        tree[UTREE][tos.k][tos.t][tos.h] = child1 * 2 + child2;
         lenq--; // pop
       } else {
-       push(1, cur->k, cur->t, cur->h, &lenq, &maxq, &stack);
-       push(0, cur->k, cur->t, cur->h - 1, &lenq, &maxq, &stack);
+        PUSH(1, tos.k, tos.t, tos.h, lenq, maxq, stack);
+        PUSH(0, tos.k, tos.t, tos.h - 1, lenq, maxq, stack);
       }
     } else {
-      assert (false);
-    }    
+      assert(false);
+    }
   }
 
   unsigned total = tree[UTREE][k][t][h];
@@ -104,6 +104,8 @@ void strahler_tree(int k, int t, int h) {
   // Epilogue
   free(stack);
   free(tree);
+
+  return total;
 }
 
 int main(int argc, char *argv[]) {
@@ -154,7 +156,7 @@ int main(int argc, char *argv[]) {
   }
 
   // Continuing with the actual computation of the
-  strahler_tree(k, t, h);
+  unsigned nleaves = strahler_tree(k, t, h);
 
   return EXIT_SUCCESS;
 }
